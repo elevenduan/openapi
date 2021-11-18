d3299cba81727938d44c0316db999ab11775fc99
段飞祥|feixiang duan <feixiang.duan@99bill.com>
2021-11-18 15:39:56 +0800

    openapi生成service文件

---
+++ bin/openapi.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const path = require("path");
+const { generateFile, generateConfigFile } = require("../lib");
+
+function run() {
+  const [runType, fileDir = "openapi"] = process.argv.slice(2);
+
+  if (runType === "init") {
+    generateConfigFile(fileDir);
+  } else {
+    const config = require(path.join(
+      process.cwd(),
+      fileDir,
+      "openapi.config.js"
+    ));
+    generateFile(config);
+  }
+}
+
+run();
